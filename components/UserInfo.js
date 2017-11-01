eeb5e759522e3cafccd93902adaf4c9e2743eefd
Max Yokha <max.yokha@gmail.com>
2017-11-02 00:21:17 +0200

    part 11 finish

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react'
 
 class UserInfo extends Component {
 
-  handleNewId() {
-    this.props.createNewUserId()
+  handleNewId () {
+    this.props.actions.createNewUserId()
+  }
+
+  handleNewIdIfOdd () {
+    this.props.actions.createNewUserIdIfOdd()
+  }
+
+  handleNewIdAsync () {
+    this.props.actions.createNewUserIdAsync()
   }
 
   render() {
@@ -12,6 +20,8 @@ class UserInfo extends Component {
         <div>username: {this.props.user.username}</div>
         <div>id: {this.props.user.id}</div>
         <button onClick={this.handleNewId.bind(this)}>Update with random ID</button>
+        <button onClick={this.handleNewIdIfOdd.bind(this)}>Update only if Odd</button>
+        <button onClick={this.handleNewIdAsync.bind(this)}>Update async</button>
       </li>
     )
   }
